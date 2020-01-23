7a1f53e90a07f8a1d2a495cf22c32be28f66df65
marinte <gabrigmt98@gmail.com>
2020-01-23 02:58:46 +0000

    fixed bug with nav current section colour.

--- js/init.js
+++ js/init.js
@@ -49,7 +49,9 @@
 			active_section = $(this);
 			if (direction === "up") active_section = active_section.prev();
 
-			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+         var name = active_section.attr("id");
+         if(name == "resume") name = "portfolio";
+			var active_link = $('#nav-wrap a[href="#' + name + '"]');
 
          navigation_links.parent().removeClass("current");
 			active_link.parent().addClass("current");
